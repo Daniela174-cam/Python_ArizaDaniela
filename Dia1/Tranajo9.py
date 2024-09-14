781882d23c536cde12a75abf58768e3a8f97f207
Daniela174-cam <danielamendez0023@gmail.com>
2024-09-14 18:44:06 -0500

    feat: :sparkles: tabajo 9 terminado

--- Dia1/Tranajo9.py
+++ Dia1/Tranajo9.py
@@ -1,2 +1,13 @@
 # Solicita la edad de la persona e indica si es niño (0-12 años), adolescente (13-17 años), adulto (18-
 # 64 años) o anciano (65 años o más).
+edad=int(input("ingrese su edad: "))
+if 0 <= edad <= 12:
+    print("Es niño")
+elif 13 <= edad <= 17:
+    print("es adolecente")
+elif 18 <= edad <= 64:
+    print("adulto")
+else:
+    print("anciano")
+
+    
