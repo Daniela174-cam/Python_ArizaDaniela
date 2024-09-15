94ef08e9a233b71f6cd672e96676999515f2a6b7
Daniela174-cam <danielamendes0023@gmail.com>
2024-09-15 17:52:00 -0500

    feat: :sparkles: Ejercicio 20 terminado

---
+++ Dia1/Trabajo18.py
@@ -0,0 +1,4 @@
+#Solicita al usuario ingresar el número de materias que ha cursado. Para cada materia, solicita el
+# puntaje y determina si ha aprobado o no (>= 60). Luego, calcula el número total de créditos del
+#estudiante (cada materia aprobada otorga 3 créditos).
+
