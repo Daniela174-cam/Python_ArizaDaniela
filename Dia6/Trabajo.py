bc8ae45c7a5659a47089020c84b1a35c89a514b1
Daniela174-cam <danielamendez0023@gmail.com>
2024-09-23 08:08:24 -0500

    feat: :sparkles:no se ha terminadooo

--- Dia6/Trabajo.py
+++ Dia6/Trabajo.py
@@ -1,18 +1,17 @@
 from jugadores import getPlayers, insertPlayer
 from partidos import getPartidos, insertMatch
+from equipoT import cuerpoTecnico,insertPlayer 
 
 def menuPrincipal():
     while True:
         try:
             opcion=input ("""            
-▗▖   ▗▄▄▄▖ ▗▄▄▖ ▗▄▖     ▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖▗▄▄▖ ▗▖    ▗▄▖▗▖  ▗▖
-▐▌     █  ▐▌   ▐▌ ▐▌    ▐▌ ▐▌▐▌     █  ▐▌ ▐▌▐▌   ▐▌ ▐▌▝▚▞▘ 
-▐▌     █  ▐▌▝▜▌▐▛▀▜▌    ▐▛▀▚▖▐▛▀▀▘  █  ▐▛▀▘ ▐▌   ▐▛▀▜▌ ▐▌  
-▐▙▄▄▖▗▄█▄▖▝▚▄▞▘▐▌ ▐▌    ▐▙▄▞▘▐▙▄▄▖  █  ▐▌   ▐▙▄▄▖▐▌ ▐▌ ▐▌  
+      LIGA BETPLAY
                                                                                       
     Opciones disponibles:
     1. menu jugadores.
-    2. menu partidos
+    2. menu partidos.
+    3. menu cuerpo tecnico.
                                                            
 
 seleccione una opcion: """)
@@ -20,6 +19,8 @@ seleccione una opcion: """)
                 menuJugadores()
             elif (opcion =='2'):
                 menuPartidos()
+            elif (opcion == '3'):
+                menuTecnico()
             else:
                 print("opcion no valida ")
         except:
@@ -29,17 +30,11 @@ seleccione una opcion: """)
 def menuJugadores():
     
     opcion = input("""
-                   
-▗▖  ▗▖▗▄▄▄▖▗▖  ▗▖▗▖ ▗▖       ▗▖▗▖ ▗▖ ▗▄▄▖ ▗▄▖ ▗▄▄▄  ▗▄▖ ▗▄▄▖ ▗▄▄▄▖ ▗▄▄▖
-▐▛▚▞▜▌▐▌   ▐▛▚▖▐▌▐▌ ▐▌       ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌▐▌  █▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌   
-▐▌  ▐▌▐▛▀▀▘▐▌ ▝▜▌▐▌ ▐▌       ▐▌▐▌ ▐▌▐▌▝▜▌▐▛▀▜▌▐▌  █▐▌ ▐▌▐▛▀▚▖▐▛▀▀▘ ▝▀▚▖
-▐▌  ▐▌▐▙▄▄▖▐▌  ▐▌▝▚▄▞▘    ▗▄▄▞▘▝▚▄▞▘▝▚▄▞▘▐▌ ▐▌▐▙▄▄▀▝▚▄▞▘▐▌ ▐▌▐▙▄▄▖▗▄▄▞▘
-                                                                       
-                                                                                                                       
-Ingrese una opcion:
+    MENU JUGADORES
+
     1. obtener todos los jugadores.
     2. insertar un nuevo jugador.
-""")
+Ingrese una opcion: """)
     try:
         opcion=int(opcion)
         if opcion == 1:
@@ -58,19 +53,14 @@ Ingrese una opcion:
 
 def menuPartidos():
     opcion = input("""
-
-▗▄▄▖  ▗▄▖ ▗▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▄  ▗▄▖  ▗▄▄▖
-▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌ █    █  ▐▌  █▐▌ ▐▌▐▌   
-▐▛▀▘ ▐▛▀▜▌▐▛▀▚▖ █    █  ▐▌  █▐▌ ▐▌ ▝▀▚▖
-▐▌   ▐▌ ▐▌▐▌ ▐▌ █  ▗▄█▄▖▐▙▄▄▀▝▚▄▞▘▗▄▄▞▘
-                                                                                                                     
+                                                                          
     Menu Partidos
                    
-Ingrese una opcion: 
     1. Obtener partidos.
     2. Insertar Partido
                    
-""")
+
+Ingrese una opcion: """)
     
     try: 
         opcion = int(opcion)
@@ -78,7 +68,24 @@ Ingrese una opcion:
     except ValueError:
         print('ingrese opcion valida pirobo')
         
+def menuTecnico():
+    
+    opcion = input("""
+    CUERPO TECNICO                                                                                                                                                     
 
+    1. obtener todo el cuerpo tecnico.
+    2. insertar nuevo tecnico.
+
+Ingrese una opcion: """)
+    try:
+        opcion=int(opcion)
+        if opcion == 1:
+            cuerpoTecnico()
+        elif opcion == 2:
+            insertPlayer()
+    except ValueError:
+        print('acaso no lee las opciones imbecil?')
+        menuTecnico()
 
 menuPrincipal()
 
