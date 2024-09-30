b715ae85227fed8cf8243d87915f52263e7f5825
Daniela174-cam <danielamendez0023@gmail.com>
2024-09-30 18:27:53 -0500

    feat: :sparkles: no se ha terminado

--- Dia8/plantel.py
+++ Dia8/plantel.py
@@ -1,14 +1,46 @@
 
 
 def plantel(lista):
+    nombreE = input("Ingrese el nombre del jugador que desea eliminar: ")
+    jugador={
+                "id": lista[-1]["id"] + 1, 
+                "nombre": nombreE, 
+                }
+
+
+def registrarEquipo(lista):
 
     lugar=str(input("ingrese nombre del plantel: "))
     local=bool(input("es ud local s(si) N(no): "))
     fecha=(input("ingrese fecha del juego: "))
     golesAfavor=int(input("ingrese la cantidad de goles del equipo: "))
     golesEnContra=int(input("ingrese la cantidad de goles que le hicieron: "))
-    diccionario ={"lugar":lugar,"local":local,"fecha":fecha,"golesAfavor":golesAfavor,"golesEnContra": golesEnContra}
-    for i,e in diccionario.items():
-        print('-----------------')
-        print(f'{i}:{e}')
-    lista.append(diccionario)
+
+    
+    jugador_encontrado = False
+    for jugador in lista:
+
+        if nombreE['nombre'].lower() == nombreE.lower():
+            lista.remove(jugador)
+
+            print(f"El jugador {nombreE} ha sido eliminado.")
+            jugador_encontrado = True
+            break
+
+    if not jugador_encontrado:
+        print(f"No se encontró al jugador con nombre {nombreE}.")
+
+def mostrarJugadores(listaJ):
+    if not listaJ:
+        print("No hay jugadores en la lista.")
+    else:
+        print("--- Lista de Jugadores ---")
+        for jugador in listaJ:
+            print(f"Nombre: {jugador['nombre']}, "
+                  f"Dorsal: {jugador['dorsal']}, "
+                  f"Posición: {jugador['posicion']}, "
+                  f"Goles: {jugador['golesJugador']}, "
+                  f"Faltas: {jugador['faltasJugador']}, "
+                  f"Tarjetas Amarillas: {jugador['tarjetaA']}, "
+                  f"Tarjetas Rojas: {jugador['tarjetaR']}")        
+        print("--------------------------")
